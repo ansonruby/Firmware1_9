c56776d346dab2832e78c40425ba69cdaa2d3d01
ansonruby <amaander011057@gmail.com>
2021-05-11 09:49:39 -0500

    Mejoras del Firmware 1_8 install

--- Actualizador/Guardian.py
+++ Actualizador/Guardian.py
@@ -82,6 +82,7 @@ while 1:
         print 'no hay actualizaciones pendientes'
         print 'revizar otras cosas.'
 
+        """
         Formware = Leer_Archivo(29)
         Firm = Formware.split('\n')
         #print Firm[0].find("Firmware1_8")
@@ -105,7 +106,7 @@ while 1:
                         Escrivir_Estados('OK',47)   # Estado final de la instalacion
                     elif len(apache2) == 0 or len(php) == 0 or len(mysql) == 0:
                         print 'NO hay nada Instalado reiniciar'
-                        commands.getoutput('sudo reboot')
+                        #commands.getoutput('sudo reboot')
 
             else:
                 print Tiem_de_activo
@@ -127,7 +128,8 @@ while 1:
                         Escrivir_Estados('OK',47)   # Estado final de la instalacion
                     elif len(apache2) == 0 or len(php) == 0 or len(mysql) == 0:
                         print 'NO hay nada Instalado reiniciar'
-                        commands.getoutput('sudo reboot')
+                        #commands.getoutput('sudo reboot')
+        """
 
 
 
@@ -166,8 +168,8 @@ while 1:
                     commands.getoutput('sudo chmod -R 775 /var/www/html')
                     commands.getoutput('sudo chmod -R g+s /var/www/html')
                     commands.getoutput('sudo chown -R pi /var/www/html')
+                    commands.getoutput('sudo chown -R www-data:www-data /var/www')
                     commands.getoutput('sudo reboot')
-
 
 
             else:
@@ -178,113 +180,3 @@ while 1:
 
 
     #res = commands.getoutput('chmod -R 755 /home/pi/Firmware/sh/'+c)
-"""
-0. berificar si esta intalado apache2 mysql y php  si no ejecutamos el intaldor
-
-1. dar permisos al sh de intalacion Firmware
-
-"""
-
-
-"""
-# ------------------------------
-#           Constantes
-# ------------------------------
-MAC_DIRC        = 'cat /sys/class/net/eth0/address'
-MAC_DIRC        = 'cat /sys/class/net/wlan0/address'
-MAC             = commands.getoutput(MAC_DIRC)
-MAC             = MAC.replace(":","")
-ID_Tarjeta      = Generar(MAC)
-#ID_Tarjeta      = ID_Tarjeta +'5'
-# para Clonar repositorios
-Direccion_Clonada           = ' /home/pi/Actualizador/'
-Nombre_Carpeta_clonada      = 'Nueva_Actualizacion'
-Direccion_Firmware          = ' /home/pi/'
-Nombre_Carpeta_Firmware     = 'Prueba_Firmware'
-
-#print ID_Tarjeta       #ID tarjeta mejorar que quede una sola peticion
-# ejemplo de comandos
-#res2 = commands.getoutput('ifconfig')
-#print res2
-
-# ------------------------------
-#       Funciones
-# ------------------------------
-
-def Verificar_Actualizacion (Arc):
-
-    lineas =Arc.split("\n")     #separador
-
-    for linea in range(len(lineas)):
-        c = lineas[linea]
-        print c
-        if linea == 0 :
-            if ID_Tarjeta !=  c :
-                print 'NO es para mi'
-                return '0'
-        if linea == 1 :
-            C =str(Leer_Archivo(17))
-            if C.find(c) != -1 :
-                print 'ya esta actualizado'
-                return '0'
-        if linea == 2 :
-            print 'Devo actualizar'
-            return c
-
-
-def Log_Actualizador(Dato):
-    print Dato
-    Escrivir(Dato,19)
-
-# ------------------------------
-#       Funcion principal
-# ------------------------------
-
-#-----  simulacion de respuesta de Actualizacion
-
-# -2. peticion de actualizacion:
-# datos de envio del dispostivo al servidor para actualizacion:
-# ID_Dispostivo, Vercion_actual_firware.
-# guardar el resultado en el archivo 15
-# respuesta de la peticion
-
-# 0 -> volver a activar procesos
-# 1 -> Actualizar
-prueba_1= 1
-
-Arc = Leer_Archivo(15)
-
-#-1.  verificar respuesta:
-print '-1. Verificar Actualizacion'
-Resp = Verificar_Actualizacion(Arc)
-
-Resp = '1'
-
-if Resp != '0':
-
-    # my_cron = CronTab(user = 'pi')
-    Borrar(19) # borrado de memoria del proceso de actulaizacion OJO "solo pro pruebas"
-    # Desactivar_Trabajo("Chicharra")
-    # revicion de bd si hay infomacion sin procesar para guardar y limpiar
-
-    if prueba_1 == 1:
-        Log_Actualizador('0. Preparando actualizacion.')
-        Log_Actualizador('0.1 Detener Firmware Actual.')
-        res16 = Leer_Archivo(16)
-        Desactivar_Trabajos(res16)
-        Log_Actualizador('0.2 Activar vista Actualizacion.')
-        Activar_Trabajo('Actualizador')
-        Log_Actualizador('0.3 Activar proceso Actualizacion.')
-        Activar_Trabajo('Proceso_Actualizar')
-
-        Log_Actualizador('0.4 Reiniciando el Dispostivo.')
-        #commands.getoutput('sudo reboot')
-    else:
-        res16 = Leer_Archivo(16)
-        Activar_Trabajos(res16)
-        Desactivar_Trabajo('Actualizador')
-        Desactivar_Trabajo('Proceso_Actualizar')
-        #commands.getoutput('sudo reboot')
-
-
-"""
